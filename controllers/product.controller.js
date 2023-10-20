1918d17c36860fbd505f059a78e128a5fff14c6f
Angela <angelaavalos2410@gmail.com>
2023-10-19 21:15:52 -0600

    Se agrega crud product

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -1,60 +1,56 @@
-const { router } = require("../app");
-const { router } = require("../routes");
+const Product = require("../models/product.model");
 
-const Product = require("../models/product.model").Product;
-
-exports.createProduct = async(req,res) =>{
-    try{
-        let product;
-
-        product = new Product(req.body);
-
-        await product.save();
-        res.send(product);
-
-    }catch(error){
-        console.log(error);
-        res.status(500).send('Hubo un error');
-
+exports.createProduct = async (req, res) => {
+    const newProduct = new Product(req.body);
+    try {
+        await newProduct.save();
+        res.status(201).send(newProduct);
+    } catch (err) {
+        res.status(400).send(err);
     }
-}
+};
 
-exports.getProducts = async(req,res) => {
-    try{
-
+exports.getAllProducts = async (req, res) => {
+    try {
         const products = await Product.find();
-        res.json(products)
-
-    }catch(error){
-        console.log(error);
-        res.status(500).send('Hubo un error');
-
+        res.status(200).send(products);
+    } catch (err) {
+        res.status(500).send(err);
     }
-}
+};
 
-exports.updateProduct = async(req,res) =>{
-
-    try{
-
-        const{product,category, price, description} = req.body;
-        let products = await Product.findById(req.params.id);
-
-        if(!products){
-            res.status(404).json({msg: 'No existe el articulo'})
+exports.getProductById = async (req, res) => {
+    try {
+        const product = await Product.findById(req.params.id);
+        if (!product) {
+            return res.status(404).send();
         }
-
-        products.product = product;
-        products.category = category;
-        products.price = price;
-        products.description = description;
-
-        products = await Product.findOneAndUpdate({_id: req.params.id},products,{new:true})
-        res.json(articulos);
-
-
-    }catch(error){
-        console.log(error);
-        res.status(500).send('Hubo un error');
+        res.status(200).send(product);
+    } catch (err) {
+        res.status(500).send(err);
     }
-}
+};
 
+exports.updateProduct = async (req, res) => {
+    try {
+        const product = await Product.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        if (!product) {
+            return res.status(404).send();
+        }
+        res.status(200).send(product);
+    } catch (err) {
+        res.status(400).send(err);
+    }
+};
+
+exports.deleteProduct = async (req, res) => {
+    try {
+        const product = await Product.findByIdAndDelete(req.params.id);
+        if (!product) {
+            return res.status(404).send();
+        }
+        res.status(200).send(product);
+    } catch (err) {
+        res.status(500).send(err);
+    }
+};
