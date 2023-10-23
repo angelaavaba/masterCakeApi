2c6aad736a52d42ea4df51c5b7b2f63c9dbfaf2c
Angela <angelaavalos2410@gmail.com>
2023-10-22 21:39:39 -0600

    Se agrega CRUD favorites

---
+++ routes/favorite.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const {
+    addFavorite,
+    getUserFavorites,
+    removeFavorite
+} = require('../controllers/favorite.controller');
+
+router.post('/', addFavorite);
+router.get('/user/:userId', getUserFavorites);
+router.delete('/', removeFavorite); // Aquí esperamos que UserId y ProductId sean enviados en el cuerpo de la solicitud
+
+module.exports = router;
+
