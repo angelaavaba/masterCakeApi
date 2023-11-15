e516d171101434a367a9a2254e79d11529ddc3db
Angela <angelaavalos2410@gmail.com>
2023-11-14 20:13:01 -0600

    Cambios de registro

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,36 +1,50 @@
 const { router } = require("../app");
 const { route } = require("../routes");
+const jwt = require('jsonwebtoken');
+const {config} = require('../config/config');
 
 const User = require("../models/user.model").User;
 
 
-async function registrarUsuario(req,res){
+async function registrarUsuario(req, res) {
     const nombreUsuario = req.body.usrn;
     const pass = req.body.password;
+    const {fechaNacimiento, domicilio, sexo } = req.body;
+    
 
-    try{
+    try {
         const newUser = await new User({
-            username:nombreUsuario,
-            password:pass
+            username: nombreUsuario,
+            password : pass,
+            fechaNacimiento,
+            domicilio,
+            sexo
         }).save();
 
         res.json({
             obj: newUser
-        })
+        });
 
-    }catch(err){
-        console.log(err)
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Error al registrar usuario' });
     }
 }
+
 
 async function loginUsuario(req,res){
     const nombreUsuario = req.body.usrn;
     const pass = req.body.password;
 
+    console.log(nombreUsuario);
+    console.log(pass);
+
     try{
         const user = await User.findOne({username: nombreUsuario});
+        console.log(user);
         
         if(user.password == pass){
+            console.log("entro")
 
             try{
                 const nuevoToken = await jwt.sign(
@@ -45,6 +59,7 @@ async function loginUsuario(req,res){
                     })
          
             }catch(err){
+                console.log(err)
                 res.status(500).json({
                     message: "Error de autenticacion"
                 })
