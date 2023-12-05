0577b6651fd7cc59de5d2fc28daa0f09e0523390
Angela <angelaavalos2410@gmail.com>
2023-12-04 18:53:05 -0600

    Se agrega updateUser y getUser

--- routes/users.js
+++ routes/users.js
@@ -1,24 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
-const{
-  registrarUsuario
-}= require('../controllers/user.controller.js')
-
-const{
-  loginUsuario
-}= require('../controllers/user.controller.js')
-
-
-
+const { registrarUsuario, loginUsuario, getUser, updateUser } = require('../controllers/user.controller.js');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-router.post('/registrar',registrarUsuario);
-router.post('/login',loginUsuario)
+router.get('/:username', getUser); // New route to get user information
+router.post('/registrar', registrarUsuario);
+router.post('/login', loginUsuario);
+router.put('/update/:username', updateUser);
 
 module.exports = router;
+
 
